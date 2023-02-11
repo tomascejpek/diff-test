ade04684a09cf5cd5dc3f3a6552f3801ef7454fd
Tomáš Cejpek <tomas.cejpek@email.cz>
2023-02-11 16:21:44 +0100

    save history

--- diff-test.sh
+++ diff-test.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 || exit
 printf "Run script from $1\n"
 
 email=$(bash ini.sh getValue email config.local.ini)
-if [ -z $email ]; then
-  printf "Missing email\n"
-  exit 1
-fi
 url=$(bash ini.sh getValue url $1)
 if [ -z $url ]; then
   printf "Missing url\n"
@@ -21,6 +17,11 @@ fi
 sort=$(bash ini.sh getValue sort $1)
 if [ -z "$sort" ]; then
   sort=0
+fi
+enable=$(bash ini.sh getValue enable $1)
+if [ -z "$enable" ] || [ "$enable" == 0 ]; then
+  echo "Not enabled!"
+  exit 1
 fi
 params=$(bash ini.sh getValue params $1)
 grepParams=$(bash ini.sh getValue grep $1)
@@ -39,8 +40,12 @@ fi
 if [ -f "last/$name-last" ]; then
   diff=$(diff $params $name-temp last/$name-last)
   if [ "$diff" != "" ]; then
-    printf "$diff" | mail -s "The $name was modified" -aFrom:\<cejpek@mzk.cz\> "$email"
     date=$(date '+%Y-%m-%d-%H:%M:%S')
+    printf "0a\n%s\n\n.\nx\n" "$diff" | ex "diff-history/$name"
+    printf "0a\n%s\n.\nx\n" "$date" | ex "diff-history/$name"
+    if [ -n "$email" ]; then
+      printf "$diff" | mail -s "The $name was modified" -aFrom:\<cejpek@mzk.cz\> "$email"
+    fi
     cp last/$name-last diff/$name-"$date"
   fi
 else
